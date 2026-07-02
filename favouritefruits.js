30c8a96ec6119a4896a4e846626add5add36a5a4
Ved Vyas <vedcodes2312@gmail.com>
2026-07-02 18:59:41 +0530

    Create favouritefruits.js

---
+++ favouritefruits.js
@@ -0,0 +1,7 @@
+var fruits = ["banana", "orange", "mango"];
+fill(255,0,0);
+text(fruits[0],10,30);
+text(fruits[1],10,60);
+text(fruits[2],10,90);
+text("my top" +  fruits.length + "fav fruits", 10, 150);
+
