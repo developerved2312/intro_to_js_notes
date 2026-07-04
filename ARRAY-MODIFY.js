a553732c5d2e432d20ffc8549b29e74b573e409f
Ved Vyas <vedcodes2312@gmail.com>
2026-07-04 19:47:16 +0530

    Create ARRAY-MODIFY.js

---
+++ ARRAY-MODIFY.js
@@ -0,0 +1,17 @@
+var icecream = ["vanilla","chocolate","mango"];
+//               0           1          2    length=3
+console.log(icecream);
+console.log(icecream.length)
+
+// // update
+// icecream[1] = "strawberry";
+// console.log(icecream);
+
+// icecream.push("strawberry","butterscoth");
+// console.log(icecream)
+// console.log(icecream.length)
+
+
+icecream.pop()
+console.log(icecream);
+console.log(icecream.length)
